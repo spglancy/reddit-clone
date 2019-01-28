c4719ea996ff7f42870ca47b40277e8c8cb0501d
Sean <Spglancy@gmail.com>
2019-01-28 14:58:24 -0800

    posting successful

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,8 +2,16 @@ const express = require('express')
 const postRoutes = express.Router()
 const Post = require('../models/post')
 
+postRoutes.get('/', (req, res) => {
+    Post.find({}).then(posts => {
+        res.render("home", { posts });
+    }).catch(err => {
+        console.log(err.message);
+    });
+})
+
 postRoutes.post('/posts/new', (req, res) => {
-    console.log(req.body);
+    Post.create(req.body);
     res.redirect('/')
 })
 
